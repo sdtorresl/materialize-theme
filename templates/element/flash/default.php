4ff076bee0c5ae7801e445be26c3f451b79c018c
Sergio Torres <sdtorresl@unal.edu.co>
2020-05-17 17:31:26 -0500

    Add flash templates

---
+++ templates/element/flash/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var array $params
+ * @var string $message
+ */
+$class = 'message';
+if (!empty($params['class'])) {
+    $class .= ' ' . $params['class'];
+}
+if (!isset($params['escape']) || $params['escape'] !== false) {
+    $message = h($message);
+}
+?>
+<script type="text/javascript">
+M.toast({html: '<?= $message ?>', displayLength: 5000})
+</script>
---
+++ templates/element/flash/error.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var array $params
+ * @var string $message
+ */
+if (!isset($params['escape']) || $params['escape'] !== false) {
+    $message = h($message);
+}
+?>
+<script type="text/javascript">
+M.toast({html: '<?= $message ?>', classes: 'error', displayLength: 5000})
+</script>
